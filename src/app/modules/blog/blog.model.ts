d843db8ce630e41b67239849bc025f4e9bc3e450
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-10-16 01:10:35 +0600

    faq part done

--- src/app/modules/blog/blog.model.ts
+++ src/app/modules/blog/blog.model.ts
@@ -2,10 +2,15 @@ import mongoose, { Schema } from 'mongoose';
 import { IBlogDocument } from './blog.inerface';
 
 
-const blogSchema = new Schema<IBlogDocument>({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-});
+const blogSchema = new Schema<IBlogDocument>(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+  {
+    timestamps: true, 
+  }
+);
 
 const BlogModel = mongoose.model<IBlogDocument>('Blog', blogSchema);
 
--- src/app/modules/frequentlyquestions/faq.model.ts
+++ src/app/modules/frequentlyquestions/faq.model.ts
@@ -1,8 +1,13 @@
 import { model, Schema } from 'mongoose';
 
-const faqSchema = new Schema({
-  question: { type: String, required: true },
-  answer: { type: String },
-});
+const faqSchema = new Schema(
+  {
+    question: { type: String, required: true },
+    answer: { type: String },
+  },
+  {
+    timestamps: true, 
+  }
+);
 
 export default model('FAQ', faqSchema);
